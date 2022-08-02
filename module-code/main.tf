b6a16a2b15f044b15058efb6ef9d1a14c0b47e1f
sandeept1986 <sthontla.mail@gmail.com>
2022-08-02 15:40:01 +0530

    new-changes

--- module-code/main.tf
+++ module-code/main.tf
@@ -1,26 +1,26 @@
-terraform {
-  required_version = ">= 1.0.0"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 2.0"
-    }
-  }
+provider "aws"{
+  region = var.region
 }
-
-provider "aws" {
-
-    region = var.region
-  
-}
-
 module "vpc" {
-   source = "./VPC"  
+  source = "./vpc"
 
+  cidr_block           = var.vpc_cidr_block
+  enable_dns_support   = var.enable_dns_support
+  enable_dns_hostnames = var.enable_dns_hostnames
+
+  tags              = var.tags
+  tags_for_resource = var.tags_for_resource
 }
 
-module "subnet" {
-   source = "./SUBNET"
-   vpc_id = module.vpc.vpc_id
-}
+module "subnets" {
+  source = "./subnets"
 
+  vpc_id                  = module.vpc.vpc_id
+  map_public_ip_on_launch = var.map_public_ip_on_launch
+  cidr_block         = var.cidr_block
+  subnet_count       = var.subnet_count
+  availability_zones = var.availability_zones
+
+  tags              = var.tags
+  tags_for_resource = var.tags_for_resource
+}
